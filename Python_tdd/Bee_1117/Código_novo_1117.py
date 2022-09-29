f21672b6a7ec7e33578d6d231aa739d9cfaecb48
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2022-09-29 04:21:49 -0300

    Update Código_novo_1117.py

--- Python_tdd/Bee_1117/Código_novo_1117.py
+++ Python_tdd/Bee_1117/Código_novo_1117.py
@@ -1,8 +1,7 @@
 def validar_nota(num):
     media = total = i = 0
-    
     while i != 2:
-        n = float(input('digite algo: '))
+        n = float(input())
         if n >= 0 and n <= 10:
             media = (n/2) + media
             i += 1
