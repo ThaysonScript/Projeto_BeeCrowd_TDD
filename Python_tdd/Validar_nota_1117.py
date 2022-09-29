c76e730e6d6d5d3684bf9e4c44243a3bcea8e602
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2022-09-29 04:08:08 -0300

    Create Validar_nota_1117.py

---
+++ Python_tdd/Validar_nota_1117.py
@@ -0,0 +1,12 @@
+def validar_nota(num):
+    media = total = i = 0
+    
+    while i != 2:
+        n = float(input('digite algo: '))
+        if n >= 0 and n <= 10:
+            media = (n/2) + media
+            i += 1
+        else:
+            print('nota invalida')
+    print(f'media = {media:.2f}')
+    return media
