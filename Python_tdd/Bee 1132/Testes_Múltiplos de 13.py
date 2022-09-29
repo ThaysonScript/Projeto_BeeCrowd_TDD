1c53a6ae1e9e9128e601a61548394c4fc8d07d58
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2022-09-29 11:52:09 -0300

    Update Testes_Múltiplos de 13.py

--- Python_tdd/Bee 1132/Testes_Múltiplos de 13.py
+++ Python_tdd/Bee 1132/Testes_Múltiplos de 13.py
@@ -1,1 +1,10 @@
-111
+import unittest
+from app import multiplos_13
+
+class Testmultiplos_13(unittest.TestCase):
+    def test_multiplos_13(self):
+        #entrada = 100 e 200
+        self.assertEqual(multiplos_13(0), 13954)
+
+if __name__ == '__main__':
+    unittest.main()
