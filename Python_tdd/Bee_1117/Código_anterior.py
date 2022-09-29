69adbe456697a55dc9b3f9af200386abc12356de
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2022-09-29 04:19:33 -0300

    Update Código_anterior.py

--- Python_tdd/Bee_1117/Código_anterior.py
+++ Python_tdd/Bee_1117/Código_anterior.py
@@ -1,1 +1,16 @@
-8
+media = 0
+total = 0
+
+i = 0
+
+while i != 2:
+    n = float(input())
+    
+    if n >= 0 and n <= 10:
+        media = n / 2 + media
+        i = i + 1
+    else:
+        print('nota invalida')
+    
+
+print('media = {:.2f}'.format(media))
